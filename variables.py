3b1c2fe5cba83be94a02c970fd868689eec37239
Subathra p <subathra.tech@gmail.com>
2026-08-29 22:48:01 +0530

    learning 29

---
+++ variables.py
@@ -0,0 +1,13 @@
+##practice 1
+
+full_name = "sivasri prabhakaran"
+age = 18
+college = "vcew"
+department = "CSE.AI & ML"
+
+print("Full Name:",full_name)
+print("Age:",age)
+print("College:",college)
+print("Department:",department)
+print()
+
